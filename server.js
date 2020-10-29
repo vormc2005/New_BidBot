c9648d12cf648aa12cb6eb8c00212d4ab2926ab6
vormc2005 <voronov.dmitry@gmail.com>
2020-10-29 16:41:25 -0400

    heroku 10.0

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api', itemsRoutes)
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/builld'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res)=>
             res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
