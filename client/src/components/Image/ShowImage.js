ee63527f589c3473b907a16d694d1f1ed8d6bc60
vormc2005 <voronov.dmitry@gmail.com>
2020-10-29 20:04:56 -0400

    http to https

--- client/src/components/Image/ShowImage.js
+++ client/src/components/Image/ShowImage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {ApiLink} from '../config'
 
-function ShowImage({item, url}) {
+function ShowImage({item}) {
 
 
     return (
         
         <div className='product-img'>
        
-          <img src={`http://localhost:5000/api/products/photo/${item._id}`} alt={item.itemname} className="mb-3" style={{maxHeight:"100%", maxWidth: "100%"}}/>  
+          <img src={`https://localhost:5000/api/products/photo/${item._id}`} alt={item.itemname} className="mb-3" style={{maxHeight:"100%", maxWidth: "100%"}}/>  
         </div>
     )
 }
